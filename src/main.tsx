a4636db93210dfb052c78a3f51c91f39fe7f3542
kalidada18 <kalidada18@users.noreply.github.com>
2026-06-12 12:05:38 +0545

    Add global ErrorBoundary to catch React crashes

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import App from './App'
+import App from './App.tsx'
 import './index.css'
+import { ErrorBoundary } from './ErrorBoundary'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL.replace(/\/$/, '')}>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+    <ErrorBoundary>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
+        <App />
+      </BrowserRouter>
+    </ErrorBoundary>
+  </React.StrictMode>
 )
